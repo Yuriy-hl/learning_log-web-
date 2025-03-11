311a0435a08c60df9ca330c69a06eb8569cb0cd9
Yuriy-hl <ura_shakirzyanov@mail.ru>
2025-03-11 17:36:25 +0300

    init

---
+++ learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""определяет схемы URL для learning_logs"""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Домашняя страница
+    path('', views.index, name='index'),
+    #страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
